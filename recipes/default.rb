14bbd5474f58399f82cf1695eb1bbba949601627
John Kew <john.kew@socrata.com>
2013-02-25 15:49:28 -0800

    Replaced download-from-source and compile with a package resource. At Socrata we build our own deb package, so the version string in the attributes should be tweaked to match your distribution.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,76 +1,25 @@
-package "build-essential"
-package "autoconf"
-package "automake"
-package "libtool"
-package "g++"
-package "libssl-dev"
-package "tcl-dev"
-package "expat"
-package "libexpat-dev"
-package "libpcre3-dev"
-package "libcap-dev"
-
-ats_version=node[:ats][:version]
-artifact = "trafficserver-#{ats_version}.tar.bz2" 
-ats_url = "#{node[:ats][:mirror]}/#{artifact}"
-local_src = "#{node[:ats][:build_root]}/#{artifact}"
-local_src_dir = "#{node[:ats][:build_root]}/trafficserver-#{ats_version}"
-install_root = "/usr/local/trafficserver-#{ats_version}"
-
-remote_file local_src do
-  source ats_url
-  checksum node[:ats][:sha256]
-  action :nothing
-end
- 
-http_request "ats" do
-  url ats_url
-  action :head
-  if File.exists?(local_src)
-    headers "If-Modified-Since" => File.mtime(local_src).httpdate
-  end
-  notifies :create, resources(:remote_file => local_src), :immediately
+package "trafficserver" do
+  version node[:ats][:version]
+  action :install
 end
 
-execute "tar" do
- user "root"
- group "root"
- cwd node[:ats][:build_root]
-  command "tar jxf #{artifact}"
- creates local_src_dir
- action :run
-end
+ats_config = %w(cache.config header_rewrite.config log_hosts.config logs_xml.config plugin.config prefetch.config records.config remap.config ssl_multicert.config storage.config update.config vaddrs.config)
 
-script "install_trafficserver" do
-  interpreter "bash"
-  user "root"
-  cwd local_src_dir
-  code <<-EOH
-  chmod 755 ./configure
-  ./configure --prefix=#{install_root} --enable-experimental-plugins
-  make
-  make install
-  ln -sf #{install_root} #{node[:ats][:link]}
-  EOH
-  not_if {File.exist?(install_root)}
+ats_config.each do |tmpl|
+    template "/opt/trafficserver/etc/trafficserver/#{tmpl}" do
+      source "#{tmpl}.erb"
+    end
 end
 
 
-#template "/etc/init.d/trafficserver" do
-#  user "root"
-#  group "root"
-#  source "trafficserver-init.erb"
-#  mode '0755'
-#end
+directory node[:ats][:cache_dir] do
+  owner "nobody"
+  group "nogroup"
+  mode 00755
+  action :create
+end
 
-#template "/etc/default/trafficserver" do
-#  user "root"
-#  group "root"
-#  source "trafficserver.erb"
-#  mode '0644'
-#end
-
-#service "trafficserver" do
-#  supports :status => true, :restart => true, :reload => true
-#  action [:enable, :start]
-#end
+service "trafficserver" do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
